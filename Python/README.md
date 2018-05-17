bad61147d022a11a41c3d5c63c8b36cc3fbf32bc
pennythewho <pennythewho@gmail.com>
2018-05-16 21:25:51 -0700

    add spline validations and remove unnecessary extra fn

---
+++ Python/README.md
@@ -0,0 +1,5 @@
+Tested in Python 3.6
+
+Packages to install
+- `pip install scipy` - automatically installs numpy
+- `pip3 install --upgrade pip; pip3 install jupyter`  See also https://jupyter.readthedocs.io/en/latest/install.html
--- Python/loadSpline.py
+++ Python/loadSpline.py
@@ -1,5 +1,7 @@
 import scipy.io as sio
+from scipy.interpolate import BSpline
 import numpy as np
+
 
 def loadMatSpline(splineFile, splineVar=None):
     """Loads a spline from .mat format file
@@ -18,26 +20,14 @@ def loadMatSpline(splineFile, splineVar=None):
     if not splineVar in contents or not contents[splineVar]['class']=='struct':
         # TODO: throw an error indicating splineVar is missing or has wrong type
         pass
-    out = sio.loadmat(splineFile,chars_as_strings=True,variable_names=[splineVar])[splineVar]
+    raw = sio.loadmat(splineFile,chars_as_strings=True,variable_names=[splineVar])[splineVar]
     # out = h5py.File(splineFile,'r')[splineVar]
-    validateSpline(out)
-    return out
+    spd = getSplineDict(raw)
+    validateSpline(spd)
+    return spd
 
-
-def validateSpline(matSp):
-    """Checks the spline representation to make sure it has all the expected fields for evaluation
-    Throws an error if anything is missing (one at a time)
-
-    :param matSp: a dict representing the spline
-    """
-    # need to have all fields indicated in bSplineFieldType if form == B-
-    # need to have same shape for knots, number, order
-    # number of axes in coefs should be same as size of axis 2 for knots/number/order
-    # size of each axis in coefs should equal values in number
-    pass
 
 def getSplineDict(matSp):
-    # TODO change this to use a UnivariateSpline object?
     return {
         'form':     matSp[0][0][0][0],
         'knots':    np.array([kd[0] for kd in matSp[0][0][1][0]]),
@@ -46,4 +36,33 @@ def getSplineDict(matSp):
         'dim':      matSp[0][0][4][0],
         'coefs':    matSp[0][0][5]
     }
+
+
+def validateSpline(spd):
+    """Checks the spline representation to make sure it has all the expected fields for evaluation
+    Throws an error if anything is missing (one at a time)
+
+    :param matSp: a dict (output from getSplineDict) representing the spline
+    """
+    # currently only supports b-splines
+    if spd['form'] != 'B-':
+        raise ValueError('These functions currently only support b-splines.')
+    # currently only supports 1-D output
+    if spd['dim'] != 1:
+        raise ValueError('These functions currently only support 1-D values.')
+    # need to have same size for knots, number, order
+    if spd['number'].shape != spd['knots'].shape or spd['number'].shape != spd['order'].shape:
+        raise ValueError('The spline''s knots, number, and order are inconsistent.')
+    # number of dims in coefs should be same as size of number
+    if spd['number'].size != spd['coefs'].ndim:
+        raise ValueError('The spline''s coefficients are inconsistent with its number.')
+    # each dim of coefs should have as many members as indicated by number
+    if not (np.array(spd['coefs'].shape) == spd['number']).all():
+        raise ValueError('The size of at least one of the spline''s coefficients doesn''t match the corresponding number.')
+    return
+
+
+
+
+
 
