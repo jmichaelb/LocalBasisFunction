5425306ecbe6bdde3d1e71a73c2927a82bcd1264
pennythewho <pennythewho@gmail.com>
2018-05-16 21:33:31 -0700

    add validation of size of x

--- Python/evalSpline.py
+++ Python/evalSpline.py
@@ -1,7 +1,6 @@
 import scipy.interpolate as interp
 import numpy as np
 
-import loadSpline as ls
 from functools import reduce
 from operator import mul
 from itertools import chain
@@ -13,10 +12,12 @@ def evalMultivariateSpline(spd, x):
     x and spd['coefs'] must have the same number of dimensions
 
     :param x:   a Numpy n-D array with the points at which
-    :param spd: a b-spline definition as given by loadSpline.getSplineDict
+    :param spd: a b-spline definition as given by loadSpline.loadMatSpline
     :return:    a Numpy n-D array with the same shape as x, with the y values for the spline
     """
     dimCt = spd['number'].size          # size of dim coeffs
+    if dimCt != x.size:
+        raise ValueError("The dimensions of the spline do not match the dimensions of the evaluation points.")
     y = spd['coefs']
     for di in range(spd['number'].size - 1,-1,-1):
         xi = x[di]
@@ -37,7 +38,7 @@ def getNextSpline(dimIdx, dimCt, spd, coefs):
     :param spd:     The spline being evaluated
     :param coefs:   The coefficients for this dimension - for this outermost dim, this is just the coefs from the spline
                     Otherwise, this is the output from the last call to interp.splev
-    :return: The [t,c,k] spline representation for the next dimension (see splev documentation)
+    :return:        The [t,c,k] spline representation for the next dimension (see splev documentation)
     """
     li = dimCt - 1
     if li != dimIdx:
@@ -45,6 +46,8 @@ def getNextSpline(dimIdx, dimCt, spd, coefs):
     t = spd['knots'][dimIdx]
     k = spd['order'][dimIdx] - 1
     return [t,coefs,k]
+
+
 
 
 
