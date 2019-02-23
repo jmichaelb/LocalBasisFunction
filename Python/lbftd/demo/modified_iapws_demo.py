15a98ab79bef67f327f8b43e6059e02cab90af9a
pennythewho <pennespinoza+git@gmail.com>
2019-02-22 16:28:47 -0800

    add title

--- Python/lbftd/demo/modified_iapws_demo.py
+++ Python/lbftd/demo/modified_iapws_demo.py
@@ -10,14 +10,14 @@ from lbftd import statevars, loadGibbs as lg, evalGibbs as eg
 water_spline = lg.loadGibbsSpline('water_demo_iapws_modified.mat')
 
 P = np.linspace(0, 1000, num=200)       # pressure, in MPa
-T = np.linspace(0, 500, num=200)        # temperature, in K
+T = np.linspace(200, 500, num=200)        # temperature, in K
 # requested thermodynamic state variables
 # (see README for full list of available state vars or don't list any to get the full set):
 # - rho: density in kg m^-3
 # - Cp: isobaric specific heat in J kg^-1 K^-1
 # - Kt: isothermal bulk modulus in MPa
 # - alpha:  thermal expansivity in K-1
-tdstate = eg.evalSolutionGibbs(water_spline['sp'], np.array([P, T]), 'rho', 'Cp', 'Kt', 'alpha')
+tdstate = eg.evalSolutionGibbs(water_spline['sp'], np.array([P, T]), 'rho', 'Cp', 'Kt', 'alpha', failOnExtrapolate=False)
 # for full set of implemented statevars: eg.evalSolutionGibbs(water_spline['sp'], np.array([P, T]))
 
 # graph the output
@@ -31,6 +31,7 @@ rho_ax.set_ylabel('Temperature ($K$)')
 rho_ax.set_zlabel('Density ($kg\: m^{-3}$)')  # use LaTex coding in labels
 rho_surf = rho_ax.plot_surface(pP, pT, tdstate.rho)
 rho_ax.invert_yaxis()
+rho_ax.set_zlim3d(-3000, 10000)
 
 cp_ax = fig.add_subplot(222, projection='3d')
 cp_ax.set_xlabel('Pressure ($MPa$)')
@@ -46,7 +47,7 @@ kt_ax.set_zlabel('Isothermal Bulk Modulus ($MPa$)')
 kt_surf = kt_ax.plot_surface(pP, pT, tdstate.Kt)
 kt_ax.invert_yaxis()
 # IAPWS has spike for this state var.  Limit the range of z-values shown
-kt_ax.set_zlim3d(-2000, 10000)
+kt_ax.set_zlim3d(-3000, 10000)
 
 alpha_ax = fig.add_subplot(224, projection='3d')
 alpha_ax.set_xlabel('Pressure ($MPa$)')
@@ -54,6 +55,8 @@ alpha_ax.set_ylabel('Temperature ($K$)')
 alpha_ax.set_zlabel('Thermal Expansivity ($K^{-1}$)')
 alpha_surf = alpha_ax.plot_surface(pP, pT, tdstate.alpha)
 alpha_ax.invert_yaxis()
+
+fig.suptitle('IAPWS 95 below 1 GPa \n modified above 1 GPa to match DAC and shockwave data')
 
 plt.show()
 
